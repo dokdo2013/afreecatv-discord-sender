8eb308debe01a4e63789a0822ccd3c0a32ae88ff
hyeonwoojo <h_jo@ejn.gg>
2022-10-15 21:09:25 +0900

    feat: env 파일 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { DiscordModule } from './discord/discord.module';
 
 @Module({
-  imports: [DiscordModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DiscordModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,8 @@ async function bootstrap() {
     {
       transport: Transport.REDIS,
       options: {
-        host: 'localhost',
-        port: 6380,
+        host: process.env.REDIS_HOST,
+        port: parseInt(process.env.REDIS_PORT),
       },
     },
   );
